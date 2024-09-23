a35e1a6ab06b023dee37c5ae603db56b92bb7a58
Alexey Osminin <supersega@mail.ru>
2024-09-23 19:01:11 +0500

    create db, models, routers

--- app/routers/category.py
+++ app/routers/category.py
@@ -1,4 +1,14 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, status, HTTPException
+from sqlalchemy.orm import Session
+from app.backend.db_depends import get_db
+from typing import Annotated
+
+from app.models import *
+from sqlalchemy import insert
+from app.schemas import CreateCategory
+
+from slugify import slugify
+
 
 router = APIRouter(prefix='/category', tags=['category'])
 
